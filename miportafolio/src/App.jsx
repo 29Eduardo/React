9d5b2082925d07d2bf61ca6d3ac2ddd15a5c792f
Eduardo Ganchala <eduardo.ganchala@epn.edu.ec>
2025-07-04 08:30:59 -0500

    Router en el portafolio

--- miportafolio/src/App.jsx
+++ miportafolio/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NombreApellido from './components/NombreApellido'
 import DatosPersonales from './components/DatosPersonales'
 import Contacto from './components/Contacto'
@@ -11,22 +12,50 @@ import './components/Estilo.css'
 function App() {
 
   return (
-    <>
-     <div className="container my-5">
-      <div className="card shadow">
-        <div className="card-body p-5">
-          <NombreApellido />
-          <DatosPersonales />
-          <Estudios />
-          <CursosRealizados />
-          <HabilidadesBlandas />
-          <HabilidadesTecnicas />
-          <Contacto />
+  <Router>
+      <div className="container my-5">
+        <nav className="mb-4">
+          <ul className="nav nav-pills">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Inicio</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/datos">Datos Personales</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/estudios">Estudios</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/cursos">Cursos</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/blandas">Habilidades Blandas</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/tecnicas">Habilidades Técnicas</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contacto">Contacto</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="card shadow">
+          <div className="card-body p-5">
+            <Routes>
+              <Route path="/" element={<NombreApellido />} />
+              <Route path="/datos" element={<DatosPersonales />} />
+              <Route path="/estudios" element={<Estudios />} />
+              <Route path="/cursos" element={<CursosRealizados />} />
+              <Route path="/blandas" element={<HabilidadesBlandas />} />
+              <Route path="/tecnicas" element={<HabilidadesTecnicas />} />
+              <Route path="/contacto" element={<Contacto />} />
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
-    </>
-  )
+    </Router>
+    );
 }
 
 export default App
